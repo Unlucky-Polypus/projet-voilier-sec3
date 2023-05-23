58fc760bbd3483fee39591c94e19afe0204e6930
Unlucky-Polypus <58611495+Unlucky-Polypus@users.noreply.github.com>
2023-05-23 09:24:40 +0200

    Add files via upload

---
+++ projet_voilier_main/src/projet_voilier_navigator/Navigator.h
@@ -0,0 +1,47 @@
+#ifndef Navigator_h
+#define Navigator_h
+
+#include "Arduino.h"
+#include "utils.h"
+
+#define MIN_WIND_ANGLE 35.0
+#define MAX_SAFRAN_ANGLE = 30
+
+struct Point {
+  float lat;
+  float lon;
+};
+
+struct Boat {
+  Point pos;
+  float course;
+};
+
+struct CommandOutput {
+  float safran;
+  float sails;
+};
+
+class Navigator {
+  private:
+    Point buoy;
+    Point position;
+    float course;
+    float wind_angle;
+
+    // Nav functions
+    float compute_azimuth();
+    bool check_straight_line(float azimuth);
+    float compute_wind_projection(float new_course);
+    float choose_side_tack(float azimuth);
+    // Commands functions
+    float adjust_traj(float azimuth);
+    float adjust_sail(float azimuth);
+
+  public:
+    Navigator(Point buoy);
+    void setBuoy(Point buoy);
+    CommandOutput navigate(Point boat, float wind_angle, float course);
+};
+
+#endif
---
+++ projet_voilier_main/src/projet_voilier_navigator/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <Arduino.h>
+
+float recalibrate_angle_180(float angle);
+
+float recalibrate_angle_360(float angle);
+
+float convert_180_to_360(float angle);
+
+#endif
